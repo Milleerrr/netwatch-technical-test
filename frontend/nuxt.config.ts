0fbafb38fad026452a2af54d1a1ab2db7cb1892e
benmiller <109474789+Milleerrr@users.noreply.github.com>
2024-11-17 00:09:51 +0000

    Refactored all, moveis & shows to use a common MediaList component

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  compatibilityDate: '2024-11-16',
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE_URL,
